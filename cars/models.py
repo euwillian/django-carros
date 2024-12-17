843da044c25369cdf20f3f464b980a7b26ed9ae9
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-17 00:12:03 -0300

    models

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Essa herança já está pronta
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=50)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    
