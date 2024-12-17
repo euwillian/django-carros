e8408a0b1ce23cec823c6e34698cdea737d7f0db
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-17 19:51:16 -0300

    adjusts

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from cars.models import Car
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    # Cabeçalho
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    # Seção de busca
+    search_fields = ('model', 'brand')
+    
+    """Tupla com um único elemento: Em Python, para criar uma tupla com um único elemento, 
+    é necessário incluir uma vírgula no final, como em ('model',). 
+    Caso contrário, o Python tratará os parênteses apenas como agrupamento de expressão e 
+    interpretará 'model' como uma string, não uma tupla."""
+    
+admin.site.register(Car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+
 # Essa herança já está pronta
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=50)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='fk_car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    
+
+    def __str__(self):
+        """Funcao padrao de Model que retorna "Car object (1)", subscrevemos"""
+        return self.model
