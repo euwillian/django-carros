c168e75d1728280eaaa3ddc78bb7ce3f80be9ce8
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-19 00:22:15 -0300

    template

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def cars_views(request):
-    return HttpResponse("Meus Carros")
+    return render(request=request, template_name='cars.html')
