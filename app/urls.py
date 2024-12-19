d1d6afc8498621dfb78f0f1b8d7667364190d2f0
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-18 23:51:45 -0300

    views / urls

--- app/urls.py
+++ app/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', ),
+    path('cars/', cars_views),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
