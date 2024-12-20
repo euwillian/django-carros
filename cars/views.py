ca42a7fd4b67d5f4738a6e7aae0c130a73259460
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-19 23:20:15 -0300

    views

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from cars.models import Car 
+# importa meu model
 
 def cars_views(request):
+    cars = Car.objects.filter(brand=2)
+    print(cars)
+    # retonar um QuerySet
+    # select * from cars, seria basicamente essa sintaxe
+    # ao verificar no navegador e olhar no terminal é apresentado um QuerySet
+    
     return render(
         request=request, 
         template_name='cars.html', 
-        context= {'cars': {'model': 'Astra 2.0'} }
+        context= {'cars': cars }
         )
