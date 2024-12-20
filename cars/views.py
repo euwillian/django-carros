a1071b19514d289c7ecdd8e26d9b4baf30068147
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-19 22:37:10 -0300

    render

--- cars/views.py
+++ cars/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def cars_views(request):
-    return render(request=request, template_name='cars.html')
+    return render(
+        request=request, 
+        template_name='cars.html', 
+        context= {'cars': {'model': 'Astra 2.0'} }
+        )
