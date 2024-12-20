33c8f7f146ecc34efaf0bdca6ef943ee0470d2fa
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-20 00:33:58 -0300

    views

--- cars/views.py
+++ cars/views.py
@@ -3,8 +3,11 @@ from cars.models import Car
 # importa meu model
 
 def cars_views(request):
-    cars = Car.objects.filter(brand=2)
-    print(cars)
+    # cars = Car.objects.filter(brand=2)
+    # cars = Car.objects.filter(brand__name="Fiat")
+    cars = Car.objects.filter(model__contains="f")
+    # Campo__função
+    # cars = Car.object.all()
     # retonar um QuerySet
     # select * from cars, seria basicamente essa sintaxe
     # ao verificar no navegador e olhar no terminal é apresentado um QuerySet
