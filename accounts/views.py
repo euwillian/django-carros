556cd562774e0a36b3df8f1a2bad2c86eef9ac52
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-01-03 23:47:39 -0300

    html, views, urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+# Este form já está pronto no Django, basta utiliza-lo.
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        user_form  = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')
+    else:
+        user_form = UserCreationForm()
+    return render(request=request, 
+                  template_name='register.html', 
+                  context={'user_form': user_form})
