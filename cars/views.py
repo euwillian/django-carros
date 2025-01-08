1dcb18e07c909effe8c3133bbf859c8c3f832c6d
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-01-07 23:26:52 -0300

    classBasedView - refactoring

--- cars/views.py
+++ cars/views.py
@@ -1,29 +1,45 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-
+from django.views import View
 
 # importa meu model, forms, views etc
 
-def cars_views(request):
-    search = request.GET.get('search')
-    # captura a request digitada na url: /cars/?search=Fox
+# def cars_views(request):
+#     search = request.GET.get('search')
+#     # captura a request digitada na url: /cars/?search=Fox
 
-    # cars = Car.objects.filter(brand=2)
-    # cars = Car.objects.filter(brand__name="Fiat")
-    cars = Car.objects.filter(model__icontains=search).order_by('model') if search else Car.objects.all().order_by(
+#     # cars = Car.objects.filter(brand=2)
+#     # cars = Car.objects.filter(brand__name="Fiat")
+#     cars = Car.objects.filter(model__icontains=search).order_by('model') if search else Car.objects.all().order_by(
+#         '-model')
+#     # Campo__função
+#     # cars = Car.object.all()
+#     # retonar um QuerySet
+#     # select * from cars, seria basicamente essa sintaxe
+#     # ao verificar no navegador e olhar no terminal é apresentado um QuerySet
+
+#     return render(
+#         request=request,
+#         template_name='cars.html',
+#         context={'cars': cars}
+#     )
+# acima está a forma "antiga"
+
+class CarsView(View):
+    
+    def get(self, request):
+            
+        search = request.GET.get('search')
+        cars = Car.objects.filter(model__icontains=search).order_by('model') if search else Car.objects.all().order_by(
         '-model')
-    # Campo__função
-    # cars = Car.object.all()
-    # retonar um QuerySet
-    # select * from cars, seria basicamente essa sintaxe
-    # ao verificar no navegador e olhar no terminal é apresentado um QuerySet
-
-    return render(
+        
+        return render(
         request=request,
         template_name='cars.html',
         context={'cars': cars}
     )
+
 
 
 def new_car_view(request):
