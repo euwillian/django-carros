8ec12ea9ee536d091a80c986966765c2230b5b35
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-01-16 01:09:19 -0300

    adjust client.py

--- gemini_api/client.py
+++ gemini_api/client.py
@@ -1,10 +1,12 @@
 import google.generativeai as genai
+import os
 
 # Essa configuração é para utilizar o Gemini da Google para gerar a bio dos veículos automaticamente.
 # link: https://ai.google.dev/gemini-api/docs/text-generation?hl=pt-br&amp%3Bauthuser=3&amp%3Blang=python&lang=python
 
 def get_car_ai_bio(model_vehicle, brand, year):
-    genai.configure(api_key="AIzaSyAdi2JhwsA_OOL4ahq7y2VUJExA5Csf2hE")
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    # obs: Use uma variável de ambiente para a chave da API
     model = genai.GenerativeModel('gemini-1.5-flash')
     
     prompt = (
@@ -15,7 +17,7 @@ def get_car_ai_bio(model_vehicle, brand, year):
     response = model.generate_content(
     prompt,
     generation_config=genai.types.GenerationConfig(
-        max_output_tokens=1000
+        max_output_tokens=300
         )
     )
     
