f53a50bb42c2dafe2e17e0722fb8e6640acb71d6
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-01-21 00:38:59 -0300

    Adjust database -> Postgresql

--- app/settings.py
+++ app/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # configuração para utilizar o POSTGRES localmente
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
